ae3720800c02fe19fdc6ef91a4feb3b25301ae84
Sammy <sammy.cullinan@gmail.com>
2020-07-25 13:23:37 -0700

    inital setup

--- Develop/index.js
+++ Develop/index.js
@@ -1,17 +1,109 @@
-const generateMarkdown = require('generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
+const inquirer = require('inquirer');
 // array of questions for user
 const questions = [
-    
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'What is the title of your project?',
+        validate: nameInput => {
+            if(nameInput) {
+                return true;
+            } else {
+                console.log('Please enter the project name!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is your project description?',
+        validate: nameInput => {
+            if(nameInput) {
+                return true;
+            } else {
+                console.log('Enter a description!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'instructions',
+        message: 'Please enter some installation instructions',
+        validate: nameInput => {
+            if(nameInput) {
+                return true;
+            } else {
+                console.log('Please enter installation instructions!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmUsage',
+        message: 'Would you like to include usage informatin?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide some information on the usage',
+        when: ({ confirmUsage }) => confirmUsage,
+        validate: nameInput => {
+            if(nameInput) {
+                return true;
+            } else {
+                console.log('Please enter usage information!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmContribution',
+        message: 'Would you like to include contribution guidelines?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Provide some information on the contribution guidelines',
+        when: ({ confirmContribution }) => confirmContribution,
+        validate: nameInput => {
+            if(nameInput) {
+                return true;
+            } else {
+                console.log('Please enter contribution guidelines!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please enter some test instructions',
+        validate: nameInput => {
+            if(nameInput) {
+                return true;
+            } else {
+                console.log('Please enter testing instructions!');
+                return false;
+            }
+        }
+    },
 ];
-
 // function to write README file
 function writeToFile(fileName, data) {
 }
 
 // function to initialize program
 function init() {
-
+    return inquirer.prompt(questions);
 }
 
 // function call to initialize program
 init();
+
